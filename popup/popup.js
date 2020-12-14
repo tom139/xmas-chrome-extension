a4f9fe2f370dd4612bffa02de7fb17f78417fd95
tom139 <tom139@users.noreply.github.com>
2020-12-14 17:35:28 +0100

    refactor: replace tabs with spaces

--- popup/popup.js
+++ popup/popup.js
@@ -4,26 +4,26 @@
  * Save the updated configurations
  */
 function save() {
-	chrome.storage.sync.set({
-		replaceImages: document.getElementById('replaceImages').checked,
-		makeItSnow: document.getElementById('makeItSnow').checked,
-	}, function() {
-		// update the value of the submit button to 'Salvato'
-		document.getElementById('submit').value = 'Salvato';
-	});
+  chrome.storage.sync.set({
+    replaceImages: document.getElementById('replaceImages').checked,
+    makeItSnow: document.getElementById('makeItSnow').checked,
+  }, function() {
+    // update the value of the submit button to 'Salvato'
+    document.getElementById('submit').value = 'Salvato';
+  });
 }
 
 /**
  * Set the value of the inputs (checkboxes, …) to the saved ones
  */
 function restore() {
-	chrome.storage.sync.get({
-		replaceImages: false,
-		makeItSnow: false,
-	}, function(items) {
-		document.getElementById('replaceImages').checked = items.replaceImages;
-		document.getElementById('makeItSnow').checked = items.makeItSnow;
-	});
+  chrome.storage.sync.get({
+    replaceImages: false,
+    makeItSnow: false,
+  }, function(items) {
+    document.getElementById('replaceImages').checked = items.replaceImages;
+    document.getElementById('makeItSnow').checked = items.makeItSnow;
+  });
 }
 
 /**
@@ -31,7 +31,7 @@ function restore() {
  * useful to change the label of the submit button
  */
 function changed() {
-	document.getElementById('submit').value = 'Conferma';
+  document.getElementById('submit').value = 'Conferma';
 }
 
 /**
@@ -39,17 +39,17 @@ function changed() {
  * @param {*} e the event called
  */
 function enter(e) {
-	if (e.keyCode === 13) {
-		e.preventDefault();
-		save();
-	}
+  if (e.keyCode === 13) {
+    e.preventDefault();
+    save();
+  }
 }
 
 /**
  * Open the help page in a new tab
  */
 function openHelp() {
-	// open a tab with the welcome page
+  // open a tab with the welcome page
   const url = chrome.extension.getURL(`web-accessibles/welcome-page/index.html`);
   chrome.tabs.create({url: url}, function (tab) {
       console.log(`New tab launched with ${url} in ${tab}`);
--- scripts/document-end.js
+++ scripts/document-end.js
@@ -6,58 +6,58 @@ var url = '';
  * Image names of 
  */
 const imageNames = [
-	'santa-squared-2.png',
-	'santa-squared.jpg',
-	'santa-wide-2.jpg',
-	'santa-wide-3.jpg',
-	'santa-wide.jpg',
-	'grinch-portrait.jpg',
+  'santa-squared-2.png',
+  'santa-squared.jpg',
+  'santa-wide-2.jpg',
+  'santa-wide-3.jpg',
+  'santa-wide.jpg',
+  'grinch-portrait.jpg',
 ]
 
 function replace() {
-	let images = document.getElementsByTagName("img");
-	for (let i = 0; i < images.length; i++) {
-		images[i].src = url;
-	}
+  let images = document.getElementsByTagName("img");
+  for (let i = 0; i < images.length; i++) {
+    images[i].src = url;
+  }
 }
 
 function getRandomImageUrl() {
-	const imagesCount = imageNames.length;
-	const index = Math.floor(Math.random() * imagesCount);
-	return chrome.extension.getURL(`web-accessibles/images/${imageNames[index]}`);
+  const imagesCount = imageNames.length;
+  const index = Math.floor(Math.random() * imagesCount);
+  return chrome.extension.getURL(`web-accessibles/images/${imageNames[index]}`);
 }
 
 chrome.storage.sync.get({
-	replaceImages: false,
-	makeItSnow: false,
+  replaceImages: false,
+  makeItSnow: false,
 }, function(items) {
-	// create a new CSS to inject in the page
-	let css = document.createElement("style");
-	css.innerHTML = '';
+  // create a new CSS to inject in the page
+  let css = document.createElement("style");
+  css.innerHTML = '';
 
-	// if replaceImages is true, get a URL and replace all images
-	if (items.replaceImages) {
-		// get the image to use
-		url = getRandomImageUrl();
-		// replace all images with it	
-		css.innerHTML += "img { content: url(\"" + url + "\") !important; object-fit: cover !important; }\n";
+  // if replaceImages is true, get a URL and replace all images
+  if (items.replaceImages) {
+    // get the image to use
+    url = getRandomImageUrl();
+    // replace all images with it	
+    css.innerHTML += "img { content: url(\"" + url + "\") !important; object-fit: cover !important; }\n";
 
-		// replace now and then wait 3 seconds to replace
-		window.setInterval(replace, 3000);
-		replace();
-	}
+    // replace now and then wait 3 seconds to replace
+    window.setInterval(replace, 3000);
+    replace();
+  }
 
-	// if makeItSnow is true load the snow.js script
-	if (items.makeItSnow) {
-		// set update the css with margin and size
-		css.innerHTML += "html,body { margin:0; height:100%; }\n";
+  // if makeItSnow is true load the snow.js script
+  if (items.makeItSnow) {
+    // set update the css with margin and size
+    css.innerHTML += "html,body { margin:0; height:100%; }\n";
 
-		// create and append the script file
-		let link = document.createElement('script');
-		link.src = chrome.extension.getURL('web-accessibles/scripts/snow.js');
-		document.head.appendChild(link);
-	}
+    // create and append the script file
+    let link = document.createElement('script');
+    link.src = chrome.extension.getURL('web-accessibles/scripts/snow.js');
+    document.head.appendChild(link);
+  }
 
-	// append the css file
-	document.body.appendChild(css);
+  // append the css file
+  document.body.appendChild(css);
 });
